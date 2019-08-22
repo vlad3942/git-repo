968206268c73c81d95e7be27b3983a119721e52d
vlad3942 <vladikgud@mail.ru>
2019-08-23 03:03:12 +0400

    Создан класс приветствия

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+public class Welcome {
+	public void sayWelcome() {
+		System.out.println("Welcome!");
+	}
+	
+	public void sayWelcome(String name) {
+		System.out.println("Welcome " + name + "!");
+	}
+}
